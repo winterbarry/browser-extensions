29327d223fe928eacb0a700d6cd7041d21012eec
Obe Ajilima <ajilimaobe@gmail.com>
2025-09-24 17:09:34 +0100

    imported data for extension card rendering

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
-import "./style.css";
+import './style.css';
+import { initializeRender } from './render.js';
 
-import { homePage } from "./home.js";
-import { menuPage } from "./menu.js";
-import { contactPage } from "./contact.js"
-import { placeHolder } from "./placeholder.js"
-
-placeHolder();
-
-const menuButton = document.getElementById("menu");
-
-menuButton.addEventListener("click", function () {
-    const pageContent = document.getElementById("content");
-    pageContent.textContent = "";
-
-    menuPage();
-});
-
-const contactButton = document.getElementById("contact");
-
-contactButton.addEventListener("click", function () {
-    const pageContent = document.getElementById("content");
-    pageContent.textContent = "";
-
-    contactPage();
-});
-
-const homeButton = document.getElementById("home");
-
-homeButton.addEventListener("click", function () {
-    const pageContent = document.getElementById("content");
-    pageContent.textContent = "";
-
-    homePage();
+document.addEventListener('DOMContentLoaded', () => {
+  initializeRender();
 });
---
+++ src/render.js
@@ -0,0 +1,5 @@
+import data from "./data.json";
+
+export function initializeRender() {
+    console.log(data[0].name); 
+}
